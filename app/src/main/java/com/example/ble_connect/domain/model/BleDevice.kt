2710bf933d488f72046741a1a167dea8ad3a51eb
choeyujeong911 <yujeongchoe20@gmail.com>
2026-04-30 14:32:22 +0900

    BleDevice에 device 추가(계층 확실히 하기 위함)

--- app/src/main/java/com/example/ble_connect/domain/model/BleDevice.kt
+++ app/src/main/java/com/example/ble_connect/domain/model/BleDevice.kt
@@ -1,7 +1,10 @@
 package com.example.ble_connect.domain.model
 
+import android.bluetooth.BluetoothDevice
+
 data class BleDevice(
     val name: String,
-    val address: String,    // MAC 주소
-    val rssi: Int   // 신호 세기
+    val address: String,        // MAC 주소
+    val rssi: Int               // 신호 세기
+    val device: BluetoothDevice // 장치 자체
 )
