1dcb4e57e82ec00a452a946c4a8ca78f576f579f
choeyujeong911 <yujeongchoe20@gmail.com>
2026-04-30 14:33:11 +0900

    오타 수정

--- app/src/main/java/com/example/ble_connect/domain/model/BleDevice.kt
+++ app/src/main/java/com/example/ble_connect/domain/model/BleDevice.kt
@@ -5,6 +5,6 @@ import android.bluetooth.BluetoothDevice
 data class BleDevice(
     val name: String,
     val address: String,        // MAC 주소
-    val rssi: Int               // 신호 세기
+    val rssi: Int,              // 신호 세기
     val device: BluetoothDevice // 장치 자체
 )
